108d4f0c82840b27df0d208ebf5082fe958faaf0
giani1233 <gianimesapelle04@gmail.com>
2026-01-11 19:46:38 -0300

    correccion

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from './app.js';
 
 const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Servidor escuchando en puerto " + PORT);
 });
